03dcec2a0c06cb6246379b4dd50bba4ce088e224
Lee Apalat <alsapalat@multisyscorp.com>
2020-02-21 14:44:44 +0800

    add crud mng request

--- api/routes.js
+++ api/routes.js
@@ -48,10 +48,47 @@ routes.put('/my/request/:reference_number/status', (req, res) => res.json({ mess
 // ADMIN
 routes.use('/mng', require('./middleware/mng'));
 
-routes.get('/mng/request', (req, res) => res.json({ message: 'ok' }));
-routes.post('/mng/request', (req, res) => res.json({ message: 'ok' }));
-routes.put('/mng/request/:id', (req, res) => res.json({ message: 'ok' }));
-routes.delete('/mng/request/:id', (req, res) => res.json({ message: 'ok' }));
+let db_request = [
+  { id: 1, name: 'Sample Request', description: 'Lorem Ipsum', is_active: 1 },
+];
+
+routes.get('/mng/request', (req, res) => {
+  res.json(db_request);
+});
+routes.get('/mng/request/:id', (req, res) => {
+  const item = db_request.find(x => `${x.id}` === `${_.get(req, 'params.id')}`)
+  if (!item) return res.status(404).json({ message: 'Item not found' });
+  res.json(item);
+});
+routes.post('/mng/request', (req, res) => {
+  const body = _.get(req, 'body') || {};
+  if (_.isEmpty(body.name || '') || _.isEmpty(body.description || '') || typeof body.is_active !== 'number') {
+    return res.status(422).json({ message: 'name, description, is_active is required' });
+  }
+  const newData = {
+    ...req.body,
+    id: (_.get(db_request, `${db_request.length - 1}.id`) || 0) + 1,
+  };
+  db_request = db_request.concat([newData]);
+  res.json({ message: 'Created!', data: newData });
+});
+routes.put('/mng/request/:id', (req, res) => {
+  const item = db_request.find(x => `${x.id}` === `${_.get(req, 'params.id')}`)
+  if (!item) return res.status(404).json({ message: 'Item not found' });
+  const body = _.get(req, 'body') || {};
+  const newData = {
+    ...item,
+    ..._.pick(req.body, ['name', 'description', 'is_active']),
+  };
+  db_request = db_request.map(x => `${x.id}` === `${_.get(req, 'params.id')}` ? newData : x);
+  res.json({ message: 'Updated!', data: newData });
+});
+routes.delete('/mng/request/:id', (req, res) => {
+  const item = db_request.find(x => `${x.id}` === `${_.get(req, 'params.id')}`)
+  if (!item) return res.status(404).json({ message: 'Item not found' });
+  db_request = db_request.filter(x => `${x.id}` !== `${_.get(req, 'params.id')}`);
+  res.json({ message: 'Removed!' });
+});
 
 routes.get('/mng/user', (req, res) => res.json({ message: 'ok' }));
 routes.post('/mng/user', (req, res) => res.json({ message: 'ok' }));
